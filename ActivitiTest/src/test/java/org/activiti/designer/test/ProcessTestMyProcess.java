11cc83163faee45c0b36f938ce8e12790fe25ba9
guojia <guojia@zlebank.com>
2016-09-09 09:24:23 +0800

    提交测试项目

---
+++ ActivitiTest/src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
@@ -0,0 +1,27 @@
+package org.activiti.designer.test;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcessTestMyProcess {
+
+	private String filename = "D:\\zplatform_workspace\\workspace\\ActivitiTest\\src\\main\\resources\\diagrams\\ordercheck.bpmn";
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+
+	@Test
+	public void startProcess() throws Exception {
+		RepositoryService repositoryService = activitiRule.getRepositoryService();
+		repositoryService.createDeployment().addInputStream("myProcess.bpmn20.xml",
+				new FileInputStream(filename)).deploy();
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("name", "Activiti");
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess", variableMap);
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+}
